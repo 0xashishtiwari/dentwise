798617c46df2800c9e08975e766b7d2f852d257a
0xashishtiwari <ashishinrewa@gmail.com>
2026-02-27 00:48:10 +0530

    landing-page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,10 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables:{
+        colorPrimary:'#e78a53',
+        colorBackground:'#f3f4f6',
+        colorText:'#111827',
+        colorNeutral:'#6b7280',
+        borderRadius:'8px',
+      }
+    }}>
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
       >
         {children}
       </body>
